df44fa651faa67643d380038c3dbf8a2178496b3
Tallman Labs <ruperthirst@gmail.com>
2025-05-12 13:58:24 +0100

    gpt

--- Experimental/HardwareSerialMonitor_v2.0.0_TrayIcon_SRC - Copy/Program.cs
+++ Experimental/HardwareSerialMonitor_v2.0.0_TrayIcon_SRC - Copy/Program.cs
@@ -3,7 +3,7 @@ using System.Windows.Forms;
 
 namespace HardwareSerialMonitor_v2
 {
-    internal static class Program
+    static class Program
     {
         [STAThread]
         static void Main()
